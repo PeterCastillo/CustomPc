aac2bf20ac16b95d17955ca802092d5be0b89efc
PeterCastillo <peterjackcc@gmail.com>
2022-09-22 00:21:38 -0500

    Hot fix

--- src/pages/TarjetaGrafica.jsx
+++ src/pages/TarjetaGrafica.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom"
+
 const TarjetaGrafica = () => {
     return (
         <>
             <h1>Estas en TarjetaGrafica</h1>
+            <Link to={`Ram`}>Selecionar Ram</Link>
         </>
     )
 }
